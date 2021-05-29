a026a8e1c517020f1fc485d2aa07f1da4b4d5596
niloysikdar <niloysikdar30@gmail.com>
2021-05-29 22:24:00 +0530

    Styled Jokes

--- src/components/Jokes/Jokes.jsx
+++ src/components/Jokes/Jokes.jsx
@@ -1,13 +1,15 @@
+import classes from "./Jokes.module.css";
+
 const Jokes = () => {
   return (
-    <div className="main">
+    <div className={classes.main}>
       <h2>Jokes</h2>
       <h3>A streak of good laughter to make you feel better.</h3>
       <p>
         Why did the student eat his homework? Because the teacher told him it
         was a piece of cake!
       </p>
-      <div className="button">
+      <div className={classes.button}>
         <h4>Once More</h4>
       </div>
     </div>
