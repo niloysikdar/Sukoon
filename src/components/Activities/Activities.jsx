da44272d234999de177915885657630c9c28d0da
niloysikdar <niloysikdar30@gmail.com>
2021-05-29 21:56:43 +0530

    Added styling for Activities

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -1,29 +1,34 @@
 import { Activitylist } from "./Activitylist";
+import classes from "./Activities.module.css";
+import HeaderDesktop from "../Header/HeaderDesktop";
 
 const Activities = () => {
   return (
-    <div className="main">
-      {Activitylist.map((item) => {
-        return (
-          <div key={item.category}>
-            <h2>{item.category}</h2>
-            <div class="container">
-              {item.activities.map((ac) => {
-                return (
-                  <div class="box" key={ac.title}>
-                    <img src={ac.image} alt="" />
-                    <h3>{ac.title}</h3>
-                    <p>{ac.description}</p>
-                  </div>
-                );
-              })}
+    <div>
+      <HeaderDesktop />
+      <div className={classes.main}>
+        {Activitylist.map((item) => {
+          return (
+            <div key={item.category} className={classes.wrapper}>
+              <h2>{item.category}</h2>
+              <div className={classes.container}>
+                {item.activities.map((ac) => {
+                  return (
+                    <div className={classes.box} key={ac.title}>
+                      <img src={ac.image} alt="" />
+                      <h3>{ac.title}</h3>
+                      <p>{ac.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className={classes.button}>
+                <h4>View All</h4>
+              </div>
             </div>
-            <div class="button">
-              <h4>View All</h4>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
