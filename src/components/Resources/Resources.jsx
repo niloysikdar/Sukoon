9840be57ab9421d88c9d519788efe22d7c1be021
niloysikdar <niloysikdar30@gmail.com>
2021-05-29 15:33:04 +0530

    Refactored HeaderDesktop

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -2,24 +2,14 @@ import { ResoucesList } from "./ResoucesList";
 import classes from "./Resources.module.css";
 import HeaderMobile from "../Header/HeaderMobile";
 import BottomNav from "../BottomNav/BottomNav";
+import HeaderDesktop from "../Header/HeaderDesktop";
 
 const Resources = () => {
   return (
     <div>
       <HeaderMobile />
       <BottomNav />
-      <div className={classes.header_desktop}>
-        <div className={classes.logo}>
-          <h2>Sukoon</h2>
-        </div>
-        <div className={classes.options}>
-          <h2>Home</h2>
-          <h2>Favourites</h2>
-          <h2>Resources</h2>
-          <h2>Test</h2>
-          <div className={classes.profile}></div>
-        </div>
-      </div>
+      <HeaderDesktop />
       <div className={classes.main}>
         <div className={classes.categories}>
           {ResoucesList.map((item) => {
