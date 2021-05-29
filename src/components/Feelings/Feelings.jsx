c167f9e2ee07cc19285fc146c05e15e0295899ba
niloysikdar <niloysikdar30@gmail.com>
2021-05-29 11:47:54 +0530

    Added style to Feelings

--- src/components/Feelings/Feelings.jsx
+++ src/components/Feelings/Feelings.jsx
@@ -1,13 +1,22 @@
 import { FeelingsList } from "./FeelingsList";
+import { useState } from "react";
+import "./Feelings.css";
 
 const Feelings = () => {
+  const [current, setCurrent] = useState("");
   return (
     <div className="main">
       <h2>How are you feeling today ?</h2>
       <div className="feelings">
         {FeelingsList.map((item) => {
           return (
-            <div className="feel" key={item.image}>
+            <div
+              className={current === item.tag ? "feel active" : "feel"}
+              key={item.image}
+              onClick={(e) => {
+                setCurrent(item.tag);
+              }}
+            >
               <img src={item.image} alt={item.tag} />
               <p>{item.tag}</p>
             </div>
