cd32f78f5d416297ec8a2dcc4fffa15c3c85f78e
niloysikdar <niloysikdar30@gmail.com>
2021-05-29 12:57:17 +0530

    Added Basic Layout for resources

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -1,7 +1,47 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faBell } from "@fortawesome/free-solid-svg-icons";
+import { ResoucesList } from "./ResoucesList";
+import "./Resources.css";
+
 const Resources = () => {
   return (
     <div>
-      <h2>Resources</h2>
+      <div className="header_mobile">
+        <FontAwesomeIcon icon={faArrowLeft} />
+        <FontAwesomeIcon icon={faBell} />
+      </div>
+      <div className="bottomnav_mobile">
+        <FontAwesomeIcon icon={faArrowLeft} />
+        <FontAwesomeIcon icon={faBell} />
+      </div>
+      <div className="header_desktop">
+        <div className="logo">
+          <h2>Sukoon</h2>
+        </div>
+        <div className="options">
+          <h2>Home</h2>
+          <h2>Favourites</h2>
+          <h2>Resources</h2>
+          <h2>Test</h2>
+          <div className="profile"></div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="categories">
+          {ResoucesList.map((item) => {
+            return (
+              <div className="category" key={item.tag}>
+                <img src={item.image} alt={item.tag} />
+                <h2>{item.tag}</h2>
+              </div>
+            );
+          })}
+        </div>
+        <h3 className="para">
+          To know about other mental health issues, talk to our chatbot:
+        </h3>
+        <button className="button">Go to Chatbot</button>
+      </div>
     </div>
   );
 };
