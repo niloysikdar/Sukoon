927a762530be716943f1cdbe96dcde239cd21a23
niloysikdar <niloysikdar30@gmail.com>
2021-05-29 17:38:52 +0530

    Added Depression CSS

--- src/components/Resources/Depression/Depression.jsx
+++ src/components/Resources/Depression/Depression.jsx
@@ -1,57 +1,66 @@
 import DepressionPic from "../../../assets/details/depression_details.png";
 import breathing from "../../../assets/details/breathing.png";
 import { DepressionContent } from "./DepressionContent";
+import classes from "./Depression.module.css";
+import HeaderDesktop from "../../Header/HeaderDesktop";
 
 const Depression = () => {
   return (
-    <div className="main">
-      <div className="wrapper">
-        <div className="header">
-          <h2>Depression</h2>
-          <img src={DepressionPic} alt="" />
-        </div>
-        {DepressionContent.map((item) => {
-          return (
-            <div className="content">
-              <h2 className="title">{item.title}</h2>
-              {item.description.map((des) => {
-                return <p className="dsecription">{des}</p>;
-              })}
-            </div>
-          );
-        })}
-        <div className="button_wrapper">
-          <div className="button">
-            <h3>Take Test</h3>
+    <div>
+      <HeaderDesktop />
+      <div className={classes.main}>
+        <div className={classes.wrapper}>
+          <div className={classes.header}>
+            <h2>Depression</h2>
+            <img src={DepressionPic} alt="" />
           </div>
-        </div>
-        <div className="activities">
-          <h2>Suggested Activities</h2>
-          <div className="activity_card">
-            <img src={breathing} alt="" />
-            <div className="card_content">
-              <h3>Breathing exercise</h3>
-              <p>It is a soothing exercise that relaxes mind and calms you</p>
+          {DepressionContent.map((item) => {
+            return (
+              <div className={classes.content} key={item.title}>
+                <h2 className={classes.title}>{item.title}</h2>
+                {item.description.map((des) => {
+                  return (
+                    <p className={classes.dsecription} key={des}>
+                      {des}
+                    </p>
+                  );
+                })}
+              </div>
+            );
+          })}
+          <div className={classes.button_wrapper}>
+            <div className={classes.button}>
+              <h3>Take Test</h3>
             </div>
           </div>
-          <div className="activity_card">
-            <img src={breathing} alt="" />
-            <div className="card_content">
-              <h3>Breathing exercise</h3>
-              <p>It is a soothing exercise that relaxes mind and calms you</p>
+          <div className={classes.activities}>
+            <h2>Suggested Activities</h2>
+            <div className={classes.activity_card}>
+              <img src={breathing} alt="" />
+              <div className={classes.card_content}>
+                <h3>Breathing exercise</h3>
+                <p>It is a soothing exercise that relaxes mind and calms you</p>
+              </div>
+            </div>
+            <div className={classes.activity_card}>
+              <img src={breathing} alt="" />
+              <div className={classes.card_content}>
+                <h3>Breathing exercise</h3>
+                <p>It is a soothing exercise that relaxes mind and calms you</p>
+              </div>
+            </div>
+            <div className={classes.activity_card}>
+              <img src={breathing} alt="" />
+              <div className={classes.card_content}>
+                <h3>Breathing exercise</h3>
+                <p>It is a soothing exercise that relaxes mind and calms you</p>
+              </div>
             </div>
           </div>
-          <div className="activity_card">
-            <img src={breathing} alt="" />
-            <div className="card_content">
-              <h3>Breathing exercise</h3>
-              <p>It is a soothing exercise that relaxes mind and calms you</p>
+          <div className={classes.button_wrapper}>
+            <div className={classes.button}>
+              <h3>View All</h3>
             </div>
-          </div>
-        </div>
-        <div className="button_wrapper">
-          <div className="button">
-            <h3>View All</h3>
           </div>
         </div>
       </div>
